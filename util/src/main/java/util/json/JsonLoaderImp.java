790af400b2ffa7752a5bdcec6646d25eda648c9b
Mathis RACINNE-DIVET <mathracinne@gmail.com>
2020-10-05 13:17:39 +0200

    Implemented Snaphot, SnapshotLayer and JSONLoader

--- util/src/main/java/util/json/JsonLoaderImp.java
+++ util/src/main/java/util/json/JsonLoaderImp.java
@@ -1,8 +1,8 @@
 package util.json;
 
 import org.json.JSONObject;
-import java.io.InputStream;
-import java.io.OutputStream;
+
+import java.io.*;
 
 /**
  @author Alexandre HAMON, Mathis RACINNE-DIVET, Margaux SCHNELZAUER-HENRY
@@ -13,25 +13,72 @@ import java.io.OutputStream;
 public class JsonLoaderImp implements JsonLoader {
 
   /**
-  This method loads a json object from an input stream 
-
-  @pre stream != null
-  @return a json object    
+   * @see JsonLoader
+   * This method loads a json object from an input stream
+   *
+   * @param path The path of the file to load
+   * @pre path != null
+   * @return a json object
    */
   @Override
-  public JSONObject loadJson(InputStream stream) {
-    return new JSONObject();
+  public JSONObject loadJson(String path) {
+    // The JSONObject to return
+    JSONObject jsonObject = null;
+
+    if (path != null) {
+      File jsonFile = new File(path);
+
+      // Checks if the given path leads to an actual file on the disk
+      if (jsonFile.exists()) {
+        try {
+          BufferedReader bf = new BufferedReader(new FileReader(path));
+          StringBuilder json = new StringBuilder();
+          String line = bf.readLine();
+
+          // Reading each line of the json file
+          while (line != null) {
+            json.append(line);
+            line = bf.readLine();
+          }
+
+          // Parsing the json into a JSONObject
+          jsonObject = new JSONObject(json.toString());
+
+        } catch (Exception e) {
+          e.printStackTrace();
+          System.err.println("Cannot read the file " + path);
+        }
+
+      } else {
+        System.err.println("Error : the file " + path + " does not exist !");
+      }
+    }
+    return jsonObject;
   }
 
 
   /**
-  This method saves a json object to an output stream 
-
-  @pre stream != null  
+   * This method saves a json object to an output stream
+   * @param path The path of the file to save
+   * @param jsonObj The JSON object to write in a file
+   * @pre stream != null
    */
   @Override
-  public void saveJson (OutputStream stream) {
+  public void saveJson (String path, JSONObject jsonObj) {
+    if (path != null && jsonObj != null) {
+      File jsonFile = new File(path);
 
+      if (jsonFile.exists()) {
+        jsonFile.delete();
+      }
+
+      // Try with ressource -> the output stream is automatically closed
+      try (PrintWriter pw = new PrintWriter(jsonFile);) {
+        pw.println(jsonObj.toString());
+      } catch (FileNotFoundException e) {
+        e.printStackTrace();
+      }
+    }
   }
 
 }
