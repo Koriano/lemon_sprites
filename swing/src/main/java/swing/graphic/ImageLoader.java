68187327870b2a4a740b620529a5c8569dbf8a96
Margaux <margauxschnelzauer@gmail.com>
2020-10-05 11:47:14 +0200

    add the class ImageLoader, implement ImageLoader interface

--- swing/src/main/java/swing/graphic/ImageLoader.java
+++ swing/src/main/java/swing/graphic/ImageLoader.java
@@ -1,2 +1,35 @@
-package swing.graphic;public class ImageLoader {
+package swing.graphic;
+import gui.graphic.Image;
+import javax.swing.ImageIcon;
+
+
+/**
+ * The implementation of the Image interface
+ * @author Margaux SCHNELZAUER
+ * @see gui.graphic.ImageLoader
+ */
+
+public class ImageLoader implements gui.graphic.ImageLoader {
+
+    /**
+     * This method load an image from a path, using an imageIcon.
+     *
+     *
+     * @param path : the image path
+     * @return the loaded image
+     */
+    @Override
+    public Image loadImage(String path) {
+
+        //create an imageIcon loaded an image from a path
+        ImageIcon image = new ImageIcon(path);
+
+        // get imageIcon properties
+        int height = image.getIconHeight();
+        int width = image.getIconWidth();
+        String name = image.getDescription();
+
+        // create and return  an Image from the ImageIcon
+        return new swing.graphic.Image(height, width, name);
+    }
 }
