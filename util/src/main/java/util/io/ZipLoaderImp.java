e4680ae6498f32c5295c68fab583c9b963d4f686
Alexandre HAMON <hamon.e1900714@etud.univ-ubs.fr>
2020-10-12 21:34:27 +0200

    ZipLoader implemented

--- util/src/main/java/util/io/ZipLoaderImp.java
+++ util/src/main/java/util/io/ZipLoaderImp.java
@@ -1,13 +1,47 @@
 package util.io;
 
+import org.json.JSONObject;
+import util.json.JsonConverter;
+import util.json.JsonLoaderImp;
+
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * @author Alexandre HAMON, Mathis RACINNE-DIVET, Margaux SCHNELZAUER-HENRY
  *
  * A generic Loader of a generic type from an input stream containing a zip file
  */
-public class ZipLoaderImp<T> implements ZipLoader<T> {
+public class ZipLoaderImp<T,R> implements ZipLoader<T> {
+
+    private static String IMG_DIRECTORY = "sprites.swing/img/";
+
+    /**
+     * The jsonConverter with already loaded images
+     */
+    private JsonConverter<T> jsonConverter;
+
+
+    private Loader<R> resourceLoader;
+
+    /**
+     * The constructor of the ZipLoaderImp class, taking a jsonConverter as an argument
+     *
+     * @pre jsonConverter != null && resourceLoader != null
+     *
+     * @param jsonConverter: the jsonConverter which contains loaded images
+     */
+    public ZipLoaderImp(JsonConverter<T> jsonConverter, Loader<R> resourceLoader){
+
+        assert jsonConverter != null && resourceLoader != null:
+                "ZipLoaderImp#constructor: precondition violated";
+
+        this.jsonConverter = jsonConverter;
+        this.resourceLoader = resourceLoader;
+
+    }
 
     /**
      * Load object from an input stream containing a zip file
@@ -19,7 +53,67 @@ public class ZipLoaderImp<T> implements ZipLoader<T> {
      */
     public T load(InputStream stream){
 
+        // Precondition
+        assert stream != null: "ZipLoader#load: precondition violated";
 
-        return null;
+        // Casting stream into ZipInputStream to use specific methods
+        ZipInputStream zis = new ZipInputStream(stream);
+
+        // Initializing entry convertion
+        ArrayList<R> resources = new ArrayList<>();
+        JsonLoaderImp json_loader = new JsonLoaderImp();
+        ZipEntry entry;
+        String ext;
+        JSONObject jsonContent = new JSONObject();
+
+        // Iterating over zip entries to load json and resources
+        do {
+            try {
+                // Get the next zip entry
+                entry = zis.getNextEntry();
+
+                // if it's not a directory
+                if(!entry.isDirectory()){
+
+                    // get its extension
+                    ext = this.extension(entry);
+
+                    // If json file, laod it with jsn loader, else it'es a resource (load it with resource loader)
+                    if ("json".equals(ext)){
+                        jsonContent = json_loader.loadJson(zis);
+                    }
+                    else{
+                        resources.add(this.resourceLoader.load(zis));
+                    }
+                }
+
+            }catch (Exception e){
+                entry = null;
+            }
+        }while (entry != null);
+
+        // Create a new object from json file
+        return this.jsonConverter.convertFromJson(jsonContent);
+    }
+
+
+    /**
+     * A private method used to return the extension of the ZipEntry
+     *
+     * @pre entry != null
+     *
+     * @param entry: the zip entry to check the extension
+     * @return the extension of the file
+     */
+    private String extension(ZipEntry entry){
+        String entryName = entry.getName();
+
+        // Split the name with slashes : /foo/bar/file.txt -> ["foo", "bar", "file.txt"]
+        String[] slash_split = entryName.split("/");
+        // Split the file name with dot : file.txt -> ["file", "txt"]
+        String[] dot_split = slash_split[slash_split.length-1].split(".");
+
+        // Return the extension
+        return dot_split[dot_split.length-1];
     }
 }
