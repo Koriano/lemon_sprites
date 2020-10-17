14fb59998608671129209e1fc17f7d4f8dae8fa2
Mathis RACINNE-DIVET <mathracinne@gmail.com>
2020-10-17 13:57:59 +0200

    Removed maven files

--- build.cmd
+++
@@ -1,4 +0,0 @@
-REM Authors : HAMON Alexandre, RACINNE-DIVET Mathis, SCHNELZAUER-HENRY Margaux
-
-call mvn package
-pause
--- clean.cmd
+++
@@ -1,4 +0,0 @@
-REM Authors : HAMON Alexandre, RACINNE-DIVET Mathis, SCHNELZAUER-HENRY Margaux
-
-call mvn clean
-pause
