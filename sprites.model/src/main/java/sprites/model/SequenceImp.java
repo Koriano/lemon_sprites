da9ef400d38622a2a901bc26c0a554594d7588ce
Margaux <margauxschnelzauer@gmail.com>
2020-11-01 22:56:27 +0100

    getCurrentSnapshot implemented

--- sprites.model/src/main/java/sprites/model/SequenceImp.java
+++ sprites.model/src/main/java/sprites/model/SequenceImp.java
@@ -28,7 +28,7 @@ public class SequenceImp implements Sequence{
     /**
      * A list of actions related to the sprites in the sequence
      */
-    private HashMap<String, SpriteAction> actions;
+    private ArrayList<SpriteAction> actions;
 
 
     /**
@@ -39,7 +39,7 @@ public class SequenceImp implements Sequence{
      * @param actions
      * @param duration
      */
-    public SequenceImp(Image background, ArrayList<Sprite> sprites, HashMap<String, SpriteAction> actions, long duration){
+    public SequenceImp(Image background, ArrayList<Sprite> sprites, ArrayList<SpriteAction> actions, long duration){
 
         this.actions = actions;
         this.background = background;
@@ -65,9 +65,95 @@ public class SequenceImp implements Sequence{
         // pre condition
         assert millis >= 0 : "Precondition violated";
 
-        
+        // get the sprites and actions array
+        ArrayList<SpriteAction> actionsList = this.actions;
+        int lengthAction = actionsList.size();
 
-        return null;
+        ArrayList<Sprite> spritesList = this.sprites;
+        int lengthSprite = spritesList.size();
+
+
+        // create an array of snapshot layer
+        SnapshotLayer[] layers = new SnapshotLayer[lengthSprite+1];
+        layers[0] = new SnapshotLayerImp(this.background, 0, 0);
+
+
+        for (int k=0; k<lengthSprite; k++){
+            // get the name of the sprite
+            String spriteName = spritesList.get(k).getName();
+
+            // search the sprites action
+            for (int i=0; i<lengthAction; i++){
+
+               SpriteAction action = actionsList.get(i);
+
+                if (action.getSprite().equals(spriteName)) {
+                    // search the good time interval
+                    if (millis < action.getEndTime() && millis > action.getStartTime()) {
+
+                        int previousX;
+                        int previousY;
+
+                        // layers parameters
+                        int X;
+                        int Y;
+                        Image image;
+
+                        //search the previous X and Y
+                        // the action is the first of the list
+                        if (i == 0) {
+                            previousX = 0;
+                            previousY = 0;
+                        }
+
+                        else {
+                            SpriteAction previouscAction = actionsList.get(i - 1);
+
+                            // check if the previous action is for the same sprite
+                            if (action.getSprite().equals(previouscAction.getSprite())) {
+                                // check if the action have a X and Y coordinate
+                                if (previouscAction.getEndX() != -1 && previouscAction.getEndY() != -1) {
+                                    previousX = previouscAction.getEndX();
+                                    previousY = previouscAction.getEndY();
+                                }
+
+                                else {
+                                    previousX = 0;
+                                    previousY = 0;
+                                }
+
+                            }
+
+                            else {
+                                previousX = 0;
+                                previousY = 0;
+                            }
+                        }
+
+                        // current coordinate calculation
+                        int deltaX = action.getEndX() - previousX;
+                        int deltaY = action.getEndY() - previousY;
+                        long deltaT = action.getEndTime() - action.getStartTime();
+
+                        X = (int) ((deltaX / deltaT) * millis + previousX);
+                        Y = (int) ((deltaY / deltaT) * millis + previousY);
+
+
+                        // search the sprite image
+                        image = spritesList.get(k).getCurrentImage(millis);
+
+                        // create and add a snapshot layer
+                        SnapshotLayer snapshotLayer = new SnapshotLayerImp(image, X, Y);
+                        layers[i] = snapshotLayer;
+                    }
+                }
+            }
+        }
+
+        // create a snapshot
+        Snapshot currentSnapshot = new SnapshotImp(layers);
+
+        return currentSnapshot;
     }
 
 
@@ -113,7 +199,6 @@ public class SequenceImp implements Sequence{
      */
     @Override
     public ArrayList<Sprite> getSprites() {
-
         return this.sprites;
     }
 
@@ -127,6 +212,6 @@ public class SequenceImp implements Sequence{
      */
     @Override
     public ArrayList<SpriteAction> getActions() {
-        return null;
+        return this.actions;
     }
 }
